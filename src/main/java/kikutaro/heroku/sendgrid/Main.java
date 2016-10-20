9fe0abcac24bc555a8fafed4cee5ae749b7b3b8d
Yoichi Kikuta <kikutaro526@yahoo.co.jp>
2016-10-20 22:47:55 +0900

    Add dependency commons-fileupload.

--- src/main/java/kikutaro/heroku/sendgrid/Main.java
+++ src/main/java/kikutaro/heroku/sendgrid/Main.java
@@ -1,6 +1,11 @@
 package kikutaro.heroku.sendgrid;
 
+import java.util.List;
 import javax.servlet.MultipartConfigElement;
+import org.apache.commons.fileupload.FileItem;
+import org.apache.commons.fileupload.FileItemIterator;
+import org.apache.commons.fileupload.disk.DiskFileItemFactory;
+import org.apache.commons.fileupload.servlet.ServletFileUpload;
 import spark.Request;
 import static spark.Spark.get;
 import static spark.Spark.port;
@@ -19,12 +24,20 @@ public class Main {
         port(Integer.valueOf(System.getenv("PORT")));
         
         get("/sendgrid", (req, res) -> "Hello SendGridder!");
+        
+        DiskFileItemFactory factory = new DiskFileItemFactory();
+        ServletFileUpload upload = new ServletFileUpload(factory);
 
         post("/sendgrid", (req, res) -> {
             System.out.println(req.body());
-            
-            MultipartConfigElement multipartConfigElement = new MultipartConfigElement("/");
-            req.attribute(org.eclipse.jetty.server.Request.__MULTIPART_CONFIG_ELEMENT ,multipartConfigElement);
+            System.out.println("リクエストのパース開始");
+            List<FileItem> items = upload.parseRequest(req.raw());
+            items.stream().forEach(fi -> {
+                if(fi.isFormField()) {
+                    System.out.println(fi.getFieldName());
+                    System.out.println(fi.getString());
+                }
+            });
             
             System.out.println("メールタイトルは " + req.raw().getPart("subject"));
             System.out.println("メール内容" + req.raw().getPart("text"));
